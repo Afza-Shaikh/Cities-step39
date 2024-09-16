74d794fa1fcdc6967ba3c88ae44bae4a23533edc
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:46:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+let Lahore = "Lahore,Pakistan";
+let Istanbul = "Istanbul,Turkey";
+let Delhi = "Delhi,India";
+function city_names(city) {
+    console.log(city);
+}
+console.log(Lahore);
